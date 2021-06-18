b5be3f87dc4e41539cf5d5866548bc362347e162
krdlab <krdlab@gmail.com>
2021-06-18 23:47:10 +0900

    chore: npm init & tsc --init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Sho Kuroda <krdlab@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+function hoge(): string {
+  return 'hoge';
+}
+
+console.log(hoge());
